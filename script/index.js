ab25ea2b29a90e3946f0c93beb767e996a9e0af4
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-08-12 23:41:32 -0700

    cleaner js

--- script/index.js
+++ script/index.js
@@ -1,7 +1,43 @@
 const manifest = require('./manifest');
 const fs = require('fs');
 const licenses = require('./licenses')
-
+// log command
+const log = (t, c) => {
+	console[t](`\n${c}\n`)
+}
+// test for sass
+try {require('sass')}catch (error) {log(`error`,`You didnt run \`npm i\``);process.exit()}
+// Compile 
+if (manifest.theme[0].endsWith('.scss') || manifest.theme[0].endsWith('.sass')) {
+	// Stop script if the css file is a sass fole
+	if (manifest.theme[1].endsWith('.scss') || manifest.theme[1].endsWith('.sass')) {
+		log(`error`,`Your css file is a ${manifest.theme[0].split('.').pop()} file and not a css file`)
+		process.exit()
+	} else {
+		if (fs.existsSync(manifest.theme[0])) {
+			const compiled = require('sass').renderSync({
+				file: manifest.theme[0],
+				sourceMap: true,
+				outFile: manifest.theme[1]
+			}).css.toString()
+			fs.writeFile(manifest.theme[1], compiled, function(err) {
+				if (err) return log(`error`,err)
+			});
+		} 
+		// Stop script if the file doesnt exist
+		else {
+			log(`error`,`${manifest.theme[0]} does not exist`)
+			process.exit()
+		}
+	}
+} else {
+	if (manifest.theme[0].endsWith('.css')) return
+    else {
+		// Stop script if its not a css or sass file
+		log(`warn`,`Since your raw file is a ${manifest.theme[0].split('.').pop()} file it cant be compiled\nIn the future it may be able to get compiled`)
+		process.exit()
+    }
+}
 // Root builder
 function BuildRoot(spr) {
 	const VersionWarning = (spr) => {
@@ -42,34 +78,15 @@ const theme = {
 if (!fs.existsSync(`./support`)) fs.mkdirSync(`./support`);
 Object.keys(theme).forEach(e => {
 	fs.writeFile(theme[e][0], theme[e][1], function(err) {
-		if (err) return console.log(err)
+		if (err) return log(`error`,err)
 	});
 })
 // Generate license
 if (!licenses[manifest.license.toLowerCase()]) {
-	console.error(`\n${manifest.license} is not a valid license or its not added yet, all the licenses added are ${Object.keys(licenses).map(file => file).join(', ')}\n`);
+	log(`error`,`${manifest.license} is not a valid license or its not added yet, all the licenses added are ${Object.keys(licenses).map(file => file).join(', ')}`)
 }
 else {
 	fs.writeFile(`./LICENSE`, licenses[manifest.license.toLowerCase()].replace('[Username]', meta.author).replace('[yyyy]', new Date().getFullYear()), function(err) {
-		if (err) return console.log(err)
+		if (err) return log(`error`,err)
 	});
-}
-// Compile 
-if (manifest.theme[0].endsWith('.scss') || manifest.theme[0].endsWith('.sass')) {
-    if (fs.existsSync(manifest.theme[0])) {
-        const compiled = require('sass').renderSync({
-            file: manifest.theme[0],
-            sourceMap: true,
-            outFile: manifest.theme[1]
-        }).css.toString()
-        fs.writeFile(manifest.theme[1], compiled, function(err) {
-            if (err) return console.log(err)
-        });
-    } 
-    else console.error(`${manifest.theme[0]} does not exist`);
-} else {
-	if (manifest.theme[0].endsWith('.css')) return
-    else {
-        console.warn(`\nSince your raw file is a ${manifest.theme[0].split('.').pop()} file it cant be compiled\nIn the future it may be able to get compiled\n`);
-    }
 }
